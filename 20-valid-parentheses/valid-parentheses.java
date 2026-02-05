f95b7cedd038558d3870832eb3ba86c1b81e714b
Ankit Kumar <146721470+Ankit6156@users.noreply.github.com>
2026-02-05 23:18:26 +0530

    Time: 3 ms (87.81%) | Memory: 43.8 MB (10.43%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if(c == ')' || c == '}' || c == ']') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if( ( c == ')' &&  top != '(') ||
+                    ( c == '}' &&  top != '{' ) ||
+                    ( c == ']' &&  top != '[') ) {
+                        return false;
+                }
+            }
+        }
+        if(!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
